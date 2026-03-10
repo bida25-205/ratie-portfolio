34e86cbf60bdc954b41c0b961318aa2c521490ce
bida25-205 <bida25-205@thuto.bac.ac.bw>
2026-03-10 17:15:23 +0200

    Added typing animation

--- script.js
+++ script.js
@@ -18,3 +18,73 @@ toggleButton.addEventListener('click', function() {
     } 
 
 }); 
+
+const texts = [ 
+
+    "Hey there!", 
+
+    "I'm Motheo a web developer", 
+
+    "Crafting digital experiences", 
+
+    "Let's build!" 
+
+]; 
+
+ 
+
+let textIndex = 0; 
+
+let charIndex = 0; 
+
+let isDeleting = false; 
+
+const typingElement = document.getElementById('typing-headline'); 
+
+ 
+
+function typeEffect() { 
+
+    const currentText = texts[textIndex]; 
+
+    if (isDeleting) { 
+
+        typingElement.textContent = currentText.substring(0, charIndex - 1); 
+
+        charIndex--; 
+
+    } else { 
+
+        typingElement.textContent = currentText.substring(0, charIndex + 1); 
+
+        charIndex++; 
+
+    } 
+
+    if (!isDeleting && charIndex === currentText.length) { 
+
+        isDeleting = true; 
+
+        setTimeout(typeEffect, 2000); 
+
+        return; 
+
+    } 
+
+    if (isDeleting && charIndex === 0) { 
+
+        isDeleting = false; 
+
+        textIndex = (textIndex + 1) % texts.length; 
+
+    } 
+
+    const speed = isDeleting ? 50 : 100; 
+
+    setTimeout(typeEffect, speed); 
+
+} 
+
+ 
+
+typeEffect(); 
